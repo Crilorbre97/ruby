3269411117696637eebc2cbcd045431cffda4d33
CRISTIAN LORCA BRENES <cristian.lorcabrenes@colaborador.elcorteingles.es>
2026-03-14 19:17:09 +0100

    add test to unmark product

--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -13,4 +13,12 @@ class FavoritesControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
     assert_not_nil Favorite.find_by(product: product.id, user: users(:sara_user).id)
   end
+
+  test "unmark product as favorite" do
+    product = products(:silla_exterior)
+    delete favorite_path(product.id)
+
+    assert_response :redirect
+    assert_nil Favorite.find_by(product: product.id, user: users(:sara_user).id)
+  end
 end
